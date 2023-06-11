cca0c0deaf6085a5683ce348c75fb11a2169baee
Eder Diego de Sousa <me@diego.dev.br>
2023-06-10 22:20:06 -0300

    Adicionando estrutura inicial backend

---
+++ backend/src/main/java/br/dev/diego/reactlogin/ReactLoginApplication.java
@@ -0,0 +1,13 @@
+package br.dev.diego.reactlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactLoginApplication.class, args);
+	}
+
+}
